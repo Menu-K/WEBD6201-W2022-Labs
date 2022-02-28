07bfff58492a9f6ea69a5880b61144a554b190b2
Menu-K <menushan.karunakaran@gmail.com>
2022-02-27 22:40:18 -0500

    Update app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,60 @@
+class User
+    {
+        // TODO: missing Getters and Setters
+
+        // constructor
+        constructor(displayName = "", emailAddress = "", username ="", password = "")
+        {
+            this.DisplayName = displayName;
+            this.EmailAddress = emailAddress;
+            this.Username = username;
+            this.Password = password;
+        }
+
+        // overriden methods
+        toString()
+        {
+            return `Display Name : ${this.DisplayName}\nEmail Address : ${this.EmailAddress}\nUsername : ${this.Username}`;
+        }
+
+        // utility methods
+        toJSON()
+        {
+            return {
+                "DisplayName": this.DisplayName,
+                "EmailAddress": this.EmailAddress,
+                "Username": this.Username
+            }
+        }
+
+        fromJSON(data)
+        {
+            this.DisplayName = data.DisplayName;
+            this.EmailAddress = data.EmailAddress;
+            this.Username = data.Username;
+            this.Password = data.Password;
+        }
+
+        serialize()
+        {
+            if(this.DisplayName !== "" && this.EmailAddress !== "" && this.Username !== "")
+            {
+                return `${this.DisplayName},${this.EmailAddress},${this.Username}`;
+            }
+            console.error("One or more properties of the User Object are missing or invalid");
+            return null;
+        }
+    
+        deserialize(data) // assume that data is in a comma-separated format (string array of properties)
+        {
+            let propertyArray = data.split(",");
+            this.DisplayName = propertyArray[0];
+            this.EmailAddress = propertyArray[1];
+            this.Username = propertyArray[2];
+        }
+    }
+
+
 // IIFE -- Immediately Invoked Function Express
 // AKA anonymous self-executing function
 
@@ -420,6 +477,7 @@
                 messageArea.removeAttr("class").hide();
             }
         });
+    }
 
         
         /**
@@ -477,10 +535,13 @@
     }
     function DisplayRegisterPage()
     {
-        RegisterFormValidation();
         console.log("Register Page");
-
-        
+        $("#contentArea").prepend(`<div id="ErrorMessage">ERROR MESSAGE</div>`);
+        $("#ErrorMessage").hide();
+        let errorMessage = $("#ErrorMessage");
+        emailAddress.text = "Email";
+        password.text = "Password";
+        RegistrationFormValidation();    
     }
 
     function Display404()
@@ -526,5 +587,4 @@
     }
     
     window.addEventListener("load", Start);
-
 })();
